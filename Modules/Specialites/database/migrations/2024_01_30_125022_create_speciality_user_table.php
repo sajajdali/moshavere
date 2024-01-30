7e87930a5bd28fd2c9298d91a0261ec745ae7b05
Sina <sina1zamani@gmail.com>
2024-01-30 15:17:25 +0330

    test

--- Modules/Specialites/database/migrations/2024_01_30_125022_create_speciality_user_table.php
+++ Modules/Specialites/database/migrations/2024_01_30_125022_create_speciality_user_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('speciality_user', function (Blueprint $table) {
             $table->id();
-            $table->fore
+            $table->foreignId('user_id')->constrained()->nullOnDelete();
+            $table->foreignId('speciality_id')->constrained()->nullOnDelete();
             $table->timestamps();
         });
     }

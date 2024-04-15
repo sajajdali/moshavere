7dbc6f4a744346569e375c63b7aa4332c2e4c594
sina <sina1zamni@gmail.com>
2024-04-15 13:49:35 +0330

    Reapply "add js file to git"
    This reverts commit b42882d2326bef8ff8600e5e23460c275cea9300.

---
+++ public/assets/admin/js/select2.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************************!*\
+  !*** ./Resources/assets/js/select2.js ***!
+  \****************************************/
+$(function (e) {
+  'use strict';
+
+  // Select2
+  $('.select2').select2({
+    minimumResultsForSearch: Infinity,
+    width: '100%'
+  });
+});
+/******/ })()
+;

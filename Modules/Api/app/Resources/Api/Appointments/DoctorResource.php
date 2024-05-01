74d0e0ee082181d7394eaf636ceb363303a840c2
sajjad <saa.mtw@gmail.com>
2024-05-01 10:22:26 +0330

    add doctor profile in api

--- Modules/Api/app/Resources/Api/Appointments/DoctorResource.php
+++ Modules/Api/app/Resources/Api/Appointments/DoctorResource.php
@@ -19,25 +19,50 @@ class DoctorResource extends JsonResource
             'speciality' => $this->getSpeciality(),
             'avatar' => $this->avatar,
             'services' => ServiceResource::collection($this->service),
-            'check_has_visited_or_not'   => true,
+            'check_has_visited_or_not' => true,
             'biography' => $this->drBiography,
             'licenceNumber' => $this->drLicenceNumber,
-            'gallery' => null,
+            'gallery' => $this->gallery(),
             'address' => $this->drAddress,
         ];
     }
+
+    private function gallery()
+    {
+        return [
+            [
+                'id' => 23,
+                'original_name' => "image 98",
+                'file_address' => 'https://amiri.selakteb.com/storage/appointment/online/1/VztbTXfMfmxuScWQp5elBuzJME0hJmUuj0791sSv.jpg',
+                'extension' => "jpg",
+                'mime' => "jpg",
+                'size' => "86.76 KB",
+                'created_at' => "1403/01/26 ساعت 20:24:10",
+            ] ,
+            [
+                'id' => 24,
+                'original_name' => "image 99",
+                'file_address' => 'https://amiri.selakteb.com/storage/appointment/online/1/VztbTXfMfmxuScWQp5elBuzJME0hJmUuj0791sSv.jpg',
+                'extension' => "jpg",
+                'mime' => "jpg",
+                'size' => "86.76 KB",
+                'created_at' => "1403/01/26 ساعت 20:24:11",
+            ]
+        ];
+    }
+
     public function getSpeciality()
     {
         $speciality = '';
-        if($this->specialities->isNotEmpty()) {
+        if ($this->specialities->isNotEmpty()) {
             foreach ($this->specialities as $special) {
                 if (strlen($speciality) < 1) {
-                    $speciality .=  $special->title;
+                    $speciality .= $special->title;
                 } else {
-                    $speciality .= ' - ' .  $special->title;
+                    $speciality .= ' - ' . $special->title;
                 }
             }
-        }else{
+        } else {
             $speciality = 'بدون تخصص';
         }
         return $speciality;

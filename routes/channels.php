1c418fd8dd6f37d9cc86b595bfb7af773b81f6a8
sajjad <saa.mtw@gmail.com>
2024-05-04 15:39:53 +0330

    add pusher

--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Modules\User\Entities\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('chat.{chatId}', function (User $user, int $chatId) {
+    return $user->id === \Modules\Chat\app\Models\Chat::find($chatId)?->user_id;
 });

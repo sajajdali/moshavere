2d0703be0fd4374cec1d34fd9d78067a7bfae2fd
sina <sina1zamani@gmail.com>
2024-07-17 14:55:39 +0330

    fix bugs

--- Modules/Setting/Helper/utils.php
+++ Modules/Setting/Helper/utils.php
@@ -1,10 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
+
 if (!function_exists('setting')) {
     function setting(?Modules\Setting\Enum\SettingKeyEnum $key)
     {
         // If $key is null, return null
         if ($key === null) {
+            return null;
+        }
+        // Check if the table exists
+        if (!Schema::hasTable('shw_settings')) {
             return null;
         }
         return \Modules\Setting\Entities\Setting::v($key);

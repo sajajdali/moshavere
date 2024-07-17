77aa654a228adb2399e8b2e1db737968f626e021
sina <sina1zamani@gmail.com>
2024-07-17 15:02:58 +0330

    update transaction migration order

--- Modules/Transaction/database/migrations/2024_07_17_150202_create_transactions_table.php
+++ Modules/Transaction/database/migrations/2024_07_17_150202_create_transactions_table.php
@@ -3,8 +3,8 @@
 use Modules\User\Entities\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 use Modules\Discount\app\Models\Discount;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {

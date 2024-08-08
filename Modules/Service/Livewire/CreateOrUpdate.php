5ac07ddcd78c5ff252542d0d826863f600e723fe
sina <sina1zamani@gmail.com>
2024-08-08 11:33:19 +0330

    fix bugs

--- Modules/Service/Livewire/CreateOrUpdate.php
+++ Modules/Service/Livewire/CreateOrUpdate.php
@@ -6,7 +6,6 @@ use Livewire\Component;
 use App\Enum\ActiveEnum;
 use Modules\User\Entities\User;
 use Spatie\Permission\Models\Role;
-use Illuminate\Support\Facades\Cache;
 use Modules\Service\app\Models\Service;
 use Modules\Service\Enum\ServiceShowTypeEnum;
 

113085f243cf213324507c58f92ebcc313968e1c
sajjad <saa.mtw@gmail.com>
2024-08-09 16:40:08 +0330

    Your commit message

--- app/Enum/RouteEnum.php
+++ app/Enum/RouteEnum.php
@@ -8,7 +8,7 @@ enum RouteEnum: string implements EnumHasDefaultInterface
 {
     case APPOINTMENT = '/appointment/{id}';
     case transaction = '/transaction/show/{id}';
-    case ONLINE_MESSAGE = '/message/{id}';
+    case ONLINE_MESSAGE = '/support/history/{id}';
     case CHAT = '/chat/{id}';
 
 
@@ -21,6 +21,7 @@ enum RouteEnum: string implements EnumHasDefaultInterface
     {
         return match ($this) {
             self::APPOINTMENT => str_replace('{id}', $replacement, $this->value),
+            self::ONLINE_MESSAGE => str_replace('{id}', $replacement, $this->value),
             default => $this->value,
         };
     }

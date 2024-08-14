bf843b47321165f8ace8e47d2a04423955ffc5ca
sina <sina1zamani@gmail.com>
2024-08-14 10:49:42 +0330

    change file path in messagedetial

--- Modules/AppointmentUser/resources/views/livewire/admin/online/message-detail.blade.php
+++ Modules/AppointmentUser/resources/views/livewire/admin/online/message-detail.blade.php
@@ -155,21 +155,21 @@
                                                             @if ($message->messageFile()->count())
                                                                 @foreach ($message->messageFile as $file)
                                                                     @if (in_array($file->mime, ['jpg','image/png', 'jpeg','image/jpeg', 'png','image/png', 'gif','image/gif', 'webp','image/webp', 'bmp','image/bmp', 'svg','image/svg' ,'tiff','image/tiff', 'heic','image/heic', 'heif','image/heif']))
-                                                                        <a href="{{ Storage::url($file->disk . $file->server_name) }}"
+                                                                        <a href="{{ Storage::url($file->disk . $file->path) }}"
                                                                             data-fancybox="gallery"
                                                                             data-caption="{{ $file->original_name }}">
-                                                                            <img src="{{ Storage::url($file->disk . $file->server_name) }}"
+                                                                            <img src="{{ Storage::url($file->disk . $file->path) }}"
                                                                                 alt="{{ $file->original_name }}"
                                                                                 style="width: 100px; height: auto;" />
                                                                         </a>
                                                                     @elseif($file->mime == 'mp3')
                                                                         <audio
-                                                                            src="{{ Storage::url($file->disk . $file->server_name) }}"
+                                                                            src="{{ Storage::url($file->disk . $file->path) }}"
                                                                             controls preload="auto"></audio>
                                                                     @endif
                                                                     <div class="main-msg-wrapper">
                                                                         <a class="text-dark"
-                                                                            href="{{ Storage::url($file->disk . $file->server_name) }}">
+                                                                            href="{{ Storage::url($file->disk . $file->path) }}">
                                                                             <span class="fs-13 mt-1"> دانلود
                                                                                 فایل</span>
                                                                             <i

ffc5f6a1e01ae0d4d337897d4c9d2644f473dc2c
sina <sina1zamani@gmail.com>
2024-08-15 10:28:22 +0330

    fix chats send file

--- Modules/Chat/Livewire/ChatView.php
+++ Modules/Chat/Livewire/ChatView.php
@@ -23,6 +23,7 @@ class ChatView extends Component
 
     public $chatMessage = '';
     public $ImgMessg;
+    public array $form ;
 
     #[Computed]
     public function chatList()
@@ -40,54 +41,60 @@ class ChatView extends Component
     {
         return Chat::firstWhere('id', $this->chatId);
     }
-    public function updatedImgMessg()
-    {
+    // public function updatedImgMessg()
+    // {
 
-        if (empty($this->ImgMessg)) {
-            $this->dispatch('error', message: 'لطفا فایل را دوباره انتخاب کنید.');
-            return;
-        }
-       $chatDetailId =  $this->chat?->chatDetails()->create([
-            'content' => $this->ImgMessg,
-            'type' => ChatDetailTypeEnum::ATTACH,
-            'user_id' => auth()->id(),
-        ]);
-            $this->InsertFileUpload($chatDetailId);
-        $this->chat?->update([
-            'status' => ChatStatusEnum::ANSWERED,
-            'new_message_by_user' => 0,
-        ]);
-        //clear input
-        $this->chatMessage = '';
-        AdminAnswerChatEvent::dispatch($this->chat);
-    }
-    private function insertFileUpload( $chatDetailId){
-        if (isset($this->ImgMessg)) {
-            $fileUrl = Storage::disk('public')->url($this->ImgMessg);
-        }
-        $p = explode('/', $this->ImgMessg);
-        // $mimeType = Storage::mimeType($this->ImgMessg);
-        $size  =  ceil((Storage::size('public/'. end($p))) / 1024);
-        $mime  =  Storage::mimeType('public/'. end($p));
-        $extension = pathinfo($fileUrl, PATHINFO_EXTENSION);
-        $fileModel = [
-            'user_id' => $this->chat?->user->id,
-            'answer_by' => auth()->user()->id,
-            'chat_detail_id' =>  $chatDetailId->id,
-            'original_name' => end($p),
-            'server_name' => end($p),
-            'disk' => 'public',
-            'path' => $this->ImgMessg,
-            'extension' => $extension,
-            'mime' => $mime,
-            'size' => $size,
-        ];
-        ChatDetailsFile::create($fileModel);
-    }
+    //     if (empty($this->ImgMessg)) {
+    //         $this->dispatch('error', message: 'لطفا فایل را دوباره انتخاب کنید.');
+    //         return;
+    //     }
+    //    $chatDetailId =  $this->chat?->chatDetails()->create([
+    //         'content' => $this->ImgMessg,
+    //         'type' => ChatDetailTypeEnum::ADMIN_MESSAGE,
+    //         'user_id' => auth()->id(),
+    //     ]);
+    //         $this->InsertFileUpload($chatDetailId);
+    //     $this->chat?->update([
+    //         'status' => ChatStatusEnum::ANSWERED,
+    //         'new_message_by_user' => 0,
+    //     ]);
+    //     //clear input
+    //     $this->chatMessage = '';
+    //     AdminAnswerChatEvent::dispatch($this->chat);
+    // }
+    // private function insertFileUpload( $chatDetailId){
+        // if (isset($this->ImgMessg)) {
+        //     $fileUrl = Storage::disk('public')->url($this->ImgMessg);
+        // }
+        // $p = explode('/', $this->ImgMessg);
+        // // $mimeType = Storage::mimeType($this->ImgMessg);
+        // $size  =  ceil((Storage::size('public/'. end($p))) / 1024);
+        // $mime  =  Storage::mimeType('public/'. end($p));
+        // $extension = pathinfo($fileUrl, PATHINFO_EXTENSION);
+        // $url = $this->ImgMessg;
+        // // Parse the URL
+        // $parsedUrl = parse_url($url);
+        // // Get the file path
+        // $filePath = str_replace('/storage/', '', $parsedUrl['path']);
+
+        // $fileModel = [
+        //     'user_id' => $this->chat?->user->id,
+        //     'answer_by' => auth()->user()->id,
+        //     'chat_detail_id' =>  $chatDetailId->id,
+        //     'original_name' => end($p),
+        //     'server_name' => end($p),
+        //     'disk' => 'public',
+        //     'path' => $filePath,
+        //     'extension' => $extension,
+        //     'mime' => $mime,
+        //     'size' => $size,
+        // ];
+        // ChatDetailsFile::create($fileModel);
+    // }
 
     public function sendMessage()
     {
-        if (empty($this->chatMessage)) {
+        if (empty($this->chatMessage) && ! isset($this->form['file'])) {
             $this->dispatch('error', message: 'متن پیام خود را وارد کنید.');
             return;
         }
@@ -95,11 +102,49 @@ class ChatView extends Component
             $this->dispatch('error', message: 'لطفا گفت و گوی مد نظر خود را انتخاب کنید');
             return;
         }
-        $chatDetail = $this->chat?->chatDetails()->create([
-            'content' => $this->chatMessage,
-            'type' => ChatDetailTypeEnum::ADMIN_MESSAGE,
-            'user_id' => auth()->id(),
-        ]);
+        if (isset($this->form['file'])) {
+            $url = $this->form['file'];
+            // Parse the URL
+            $parsedUrl = parse_url($url);
+
+            // Get the file path
+            $filePath = str_replace('/storage/', '', $parsedUrl['path']);
+            // Get the file name
+            $fileName = basename($filePath);
+
+            // Get the dsk
+            $fileDisk = 'public';
+            $extension = pathinfo($parsedUrl['path'], PATHINFO_EXTENSION);
+
+            $chatDetail =  $this->chat?->chatDetails()->create([
+                        'content' => $this->ImgMessg,
+                        'type' => ChatDetailTypeEnum::ADMIN_MESSAGE,
+                        'user_id' => auth()->id(),
+                    ]);
+            $fileModel = [
+                'user_id' => $this->chat?->user->id,
+                'answer_by' => auth()->user()->id,
+                'chat_detail_id' =>  $chatDetail->id,
+                'original_name' =>$fileName,
+                'server_name' => $fileName,
+                'disk' => $fileDisk,
+                'path' => $filePath,
+                'extension' => $extension,
+                'mime' => $extension,
+                'size' => 1,
+            ];
+            ChatDetailsFile::create($fileModel);
+            unset($this->form['file']);
+            $this->addError('success', 'پیام با موفقیت ارسال شد');
+        }else{
+            unset($this->form['typedMessage']);
+            $this->addError('success', 'پیام با موفقیت ارسال شد');
+            $chatDetail = $this->chat?->chatDetails()->create([
+                'content' => $this->chatMessage,
+                'type' => ChatDetailTypeEnum::ADMIN_MESSAGE,
+                'user_id' => auth()->id(),
+            ]);
+        }
         $this->chat?->update([
             'status' => ChatStatusEnum::ANSWERED,
             'new_message_by_user' => 0,
@@ -120,7 +165,7 @@ class ChatView extends Component
             message: '',
             link: \App\Enum\RouteEnum::ONLINE_MESSAGE->getLink($this->chat->id),
         ));
-
+        $this->dispatch('messageHasBeenSend',true);
         AdminAnswerChatEvent::dispatch($this->chat);
     }
 

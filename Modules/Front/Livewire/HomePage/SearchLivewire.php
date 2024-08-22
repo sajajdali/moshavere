5cc46ccaf6c16b55585c6488759089185d80e38e
sina <sina1zamani@gmail.com>
2024-08-22 11:59:00 +0330

    f

--- Modules/Front/Livewire/HomePage/SearchLivewire.php
+++ Modules/Front/Livewire/HomePage/SearchLivewire.php
@@ -66,7 +66,7 @@ class SearchLivewire extends Component
             if ($sanitizedInput == 'پزشکان') {
                 $doctors = User::doctors_query()->whereHas('metas', function ($query) {
                     $query->where('meta_key', UserMetaEnum::ACTIVE_APPOINTMENT)
-                          ->whereNotIn('meta_value', [false, 'false', '0', null, '']);
+                          ->whereNotIn('meta_value', [true,'true',1,'1']);
                 })->when(isset($this->filter['speciality']), function ($query) {
                     $query->whereHas('specialities', function ($qq) {
                         $qq->where('title', 'LIKE', "%{$this->filter['speciality']}%");

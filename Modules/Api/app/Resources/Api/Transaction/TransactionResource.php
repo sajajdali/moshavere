fdeebb76fe2dbbfeddca7c1b5a09087d7f83fa20
sajjad <saa.mtw@gmail.com>
2024-08-24 17:35:13 +0330

    change price in payment

--- Modules/Api/app/Resources/Api/Transaction/TransactionResource.php
+++ Modules/Api/app/Resources/Api/Transaction/TransactionResource.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Api\app\Resources\Api\Transaction;
 
+use App\Enum\RouteEnum;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Modules\Api\app\Resources\Api\Appointments\DoctorResource;
 use Modules\Api\Transformers\UserResource;
@@ -23,12 +24,32 @@ class TransactionResource extends JsonResource
             'paid_by' => $transaction->paid_by ? $transaction->paid_by->apiResult() : null,
             'transaction_date' => verta($transaction->created_at)->format('l j F Y ساعت H:i دقیه'),
             'appointment' => [
+                'tracking_url' => route('front.setAppointment.detail', ['tracking_code' => $this->tracking_code]),
                 'start_time' => substr($this->start_time , 0 , -3),
                 'end_time' => substr($this->end_time, 0 , -3),
                 'date_visit' => verta($this->date_visit)->format('%d %B %Y'),
                 'date_visit_format' => verta($this->date_visit)->format('l j F Y'),
                 'doctor' => DoctorResource::make($this->doctor)
+            ],
+            'payment' => [
+                'payment_link'
             ]
+        ];
+    }
+
+    private function payment()
+    {
+        $transaction = $this->transaction()->orderByDesc('id')->first();
+
+        if (in_array($transaction->status , [TransactionStatusEnum::REJECTED , TransactionStatusEnum::PENDING]) ){
+            return [
+                'status' => true,
+                'payment_link' => route('api.appointment.payment.create', $this),
+            ];
+        }
+        return [
+            'status' => false,
+            'payment_link' => null
         ];
     }
 }

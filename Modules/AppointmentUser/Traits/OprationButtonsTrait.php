b24d9656f267057f3651f59d618b54ee70424272
sina <sina1zamani@gmail.com>
2024-08-26 13:49:37 +0330

    send sms status for cancelApp

--- Modules/AppointmentUser/Traits/OprationButtonsTrait.php
+++ Modules/AppointmentUser/Traits/OprationButtonsTrait.php
@@ -46,7 +46,7 @@ trait OprationButtonsTrait
     }
     public function cancelAndDeleteApp($id)
     {
-        $this->cancelAppointment($id, true);
+        $this->cancelAppointment($id, false);
         $app = AppointmentUser::find($id);
         $app->delete();
         Cache::forget('appointmentList.' . $app->setting->id);

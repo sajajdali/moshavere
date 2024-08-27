d1e23dadabb981e78ebf8a611fa20fe37e25331d
sina <sina1zamani@gmail.com>
2024-08-27 18:22:09 +0330

    remove question 3 from feedback

--- Modules/Front/Livewire/FeedBack/Questions.php
+++ Modules/Front/Livewire/FeedBack/Questions.php
@@ -6,8 +6,10 @@ use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Locked;
 use Modules\User\Entities\User;
+use Modules\Front\enum\FeedbackId;
 use Modules\Front\app\Models\FeedBack;
 use Modules\AppointmentUser\app\Models\AppointmentUser;
+use Modules\AppointmentUser\Enum\AppointmentUserKindEnum;
 
 #[Layout('front::layouts.app')]
 class Questions extends Component
@@ -23,7 +25,8 @@ class Questions extends Component
 
     public $appintment_user_id;
 
-    public function messages() {
+    public function messages()
+    {
         return [
             'form.required' => 'لطفا یک گزینه را انتخاب کنید',
         ];
@@ -32,8 +35,8 @@ class Questions extends Component
     {
         $this->resetErrorBag();
         $this->validate(['form' => 'required']);
-        if(count($this->form) < count($this->fetchData['questions'] )) {
-           return $this->addError('form' , 'به تمامی پرسش ها پاسخ داده نشده است');
+        if (count($this->form) < count($this->fetchData['questions'])) {
+            return $this->addError('form', 'به تمامی پرسش ها پاسخ داده نشده است');
         }
         foreach ($this->form as $question => $answer) {
             FeedBack::create([
@@ -42,25 +45,33 @@ class Questions extends Component
                 'answer'              => $answer,
             ]);
         }
-        $this->fetchData['formAlreadyCopelete'] = true ;
+        $this->fetchData['formAlreadyCopelete'] = true;
     }
     public function mount()
     {
         $user = User::find(request()->route('user_id'));
         $app_id = request()->route('appointmentUser_id');
         $appId =  AppointmentUser::find($app_id);
-        if ($user->id != $appId->user_id ) {
+        if ($user->id != $appId->user_id) {
             abort(403, 'Unauthorized action.');
         }
 
         if (!isset($app_id) ||  empty($appId)) {
             return abort('404');
         }
-        if ($appId->feedbacks()->exists() ){
-            $this->fetchData['formAlreadyCopelete'] = true ;
+        if ($appId->feedbacks()->exists()) {
+            $this->fetchData['formAlreadyCopelete'] = true;
         }
         $this->appintment_user_id = $app_id;
         $this->fetchData['questions'] = feedbackQuestions();
+        if ($appId->kind == AppointmentUserKindEnum::ONLINE) {
+            foreach ($this->fetchData['questions'] as $index => $questionEnum) {
+                if ($questionEnum['id'] == FeedbackId::QUESTION_3) {
+                    unset($this->fetchData['questions'][$index]);
+                    break;
+                }
+            }
+        }
     }
     public function render()
     {

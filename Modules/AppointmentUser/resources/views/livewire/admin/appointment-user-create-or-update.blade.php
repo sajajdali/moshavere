4d4212d28e391eb5dfc33146ca1fd30cdc438b7b
sina <sina1zamani@gmail.com>
2024-10-01 17:04:26 +0330

    fb

--- Modules/AppointmentUser/resources/views/livewire/admin/appointment-user-create-or-update.blade.php
+++ Modules/AppointmentUser/resources/views/livewire/admin/appointment-user-create-or-update.blade.php
@@ -22,7 +22,7 @@
                                         href="#doctors" wire:ignore.self>
                                         پزشک ها
                                     </a>
-                                    @if(auth()->user()->isAdmin())
+                                    @if(auth()->user()->isAdmin() || auth()->user()->hasRole('منشی'))
                                     <a class="nav-link border border-bottom-0 br-sm-5 me-2  @if (isset($this->search['searchService'])) active @endif"
                                         data-bs-toggle="tab" href="#sections" wire:ignore.self>
                                         بخش ها

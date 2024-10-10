d810df5876088e0f9d9f7d760a35f797030fdb08
sajad <saa.mtw@gmail.comn>
2024-10-10 17:34:55 +0330

    split_number_by_two

--- Modules/Api/Notifications/AuthSmsNotification.php
+++ Modules/Api/Notifications/AuthSmsNotification.php
@@ -38,21 +38,46 @@ class AuthSmsNotification extends Notification implements ShouldQueue
         return $notifiable->mobile;
     }
 
+    private function split_number_by_two($number): string
+    {
+        // Convert the number to a string
+        $number = (string)$number;
+
+        // Get the length of the number
+        $length = strlen($number);
+
+        // Split the number into groups of two digits
+        $result = '';
+        for ($i = 0; $i < $length; $i += 2) {
+            if ($i + 2 > $length) {
+                // If the last group has only one digit
+                $result .= substr($number, $i, 1);
+            } else {
+                // Add two digits to the result
+                $result .= substr($number, $i, 2) . ' ';
+            }
+        }
+
+        // Trim any trailing space
+        return trim($result);
+    }
     /**
      * Get the array representation of the notification.
      */
     public function toArray(mixed $notifiable): array
     {
+        $param = $this->code;
         $loginTemplate = setting(SettingKeyEnum::SMS_API_LOGIN_TEMPLATE);
         if($this->type ==  2 ){
             $loginTemplate = setting(SettingKeyEnum::CALL_LOGIN_TEMPLATE);
+            $param = $this->split_number_by_two($loginTemplate);
         }
         return [
             'template' => $loginTemplate,
             'receptor' => $notifiable->mobile,
             'type'     => $this->type ,
             'params' => [
-                $this->code,
+                $param,
             ],
         ];
     }

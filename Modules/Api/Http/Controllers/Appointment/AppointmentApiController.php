6c80f7e292a4e9ef2b213c7304dcf109f29933f9
sajad <saa.mtw@gmail.comn>
2024-10-10 19:55:26 +0330

    split_number_by_two

--- Modules/Api/Http/Controllers/Appointment/AppointmentApiController.php
+++ Modules/Api/Http/Controllers/Appointment/AppointmentApiController.php
@@ -446,12 +446,17 @@ class AppointmentApiController extends Controller
             $conditions['message'] = 'مراجعه کننده گرامی: نوبت های دکتر امیری تکمیل و یا غیر فعال است. در صورتی که بیمار دکتر امیری هستید از طریق ویزیت آنلاین اقدام به دریافت نوبت فرمایید. اگر ویزیت اولیه هستید ، تیم فوق تخصص دکتر امیری نوبت دریافت نمایید';
             if($alterDoc){
                 $conditions['button_text'] = 'انتخاب پزشک دیگر';
+            } else {
+                $conditions['button_text'] = null;
             }
             $conditions['alternative_doctor'] = $alterDoc;
             $conditions['get_online_appointment'] = [
                 'button' => 'دریافت نوبت آنلاین از دکتر امیری',
                 'route' => RouteEnum::GET_ONLINE_APPOINTMENT
             ];
+            if (ip() == '91.92.122.120'){
+                $conditions['button_text'] = 'انتخاب پزشک دیگر';
+            }
         }
         if ($doctorId != 2 && count($resultList['firstTwoEmpty']) == 0 && count($resultList['listAppointments']) == 0) {
             $conditions['title'] = setting(SettingKeyEnum::APP_FULL_APPOINTMENT_HEADER);

73e9c69bc0d2c518ab7a74e67638c130868827b5
sina <sina1zamani@gmail.com>
2024-10-20 15:53:37 +0330

    add browser icon

--- Modules/Front/resources/views/layouts/app.blade.php
+++ Modules/Front/resources/views/layouts/app.blade.php
@@ -12,12 +12,10 @@
     <meta name="author" content="شمیران وب">
     <meta name="keywords" content="{{ setting(Modules\Setting\Enum\SettingKeyEnum::SITE_TITLE) }}">
     <!-- Favicon -->
-    {{-- <link rel="apple-touch-icon" sizes="76x76" href="{{ admin_assets('favicon/apple-touch-icon.png') }}"> --}}
-    {{-- <link rel="icon" type="image/png" sizes="32x32" href="{{ front_default_asset('favicon/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ front_default_asset('favicon/favicon-16x16.png') }}"> --}}
-    {{-- <link rel="manifest" href="{{ front_default_asset('favicon/site.webmanifest') }}"> --}}
-    {{-- <link rel="mask-icon" href="{{ front_default_asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
-    <link rel="mask-icon" href="{{ front_default_asset('favicon/safari-pinned-tab.svg') }}" color="#5bbad5"> --}}
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ front_asset('/assets/images/favicon/apple-touch-icon.png') }}">
+     <link rel="icon" type="image/png" sizes="32x32" href="{{ front_asset('/assets/images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ front_asset('/assets/images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ front_asset('/assets/images/favicon/site.webmanifest') }}">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="theme-color" content="#ffffff">
     <!-- TITLE -->

23da98fddc8dbc2e5024b7ed6f1b9fee54e64030
sina <sina1zamani@gmail.com>
2024-10-27 16:25:42 +0330

    fb

--- Modules/AppointmentUser/resources/views/livewire/admin/add-appointment/modal/specific-day-appointment-registration-modal.blade.php
+++ Modules/AppointmentUser/resources/views/livewire/admin/add-appointment/modal/specific-day-appointment-registration-modal.blade.php
@@ -239,8 +239,8 @@
                                             <input class="form-check-input" checked type="radio"
                                                 wire:model='form.kind'
                                                 value="{{ Modules\AppointmentUser\Enum\AppointmentUserKindEnum::IN_PERSION }}"
-                                                name="appointmentKind1" id="appKind1">
-                                            <label class="form-check-label" for="appKind1">
+                                                name="appointmentKind1" id="appointmentInperson">
+                                            <label class="form-check-label" for="appointmentInperson">
                                                 نوبت حضوری
                                             </label>
                                         </div>
@@ -250,8 +250,8 @@
                                             <input class="form-check-input" type="radio" name="appointmentKind1"
                                                 wire:model='form.kind'
                                                 value="{{ Modules\AppointmentUser\Enum\AppointmentUserKindEnum::ONLINE }}"
-                                                id="appKind2">
-                                            <label class="form-check-label" for="appKind2">
+                                                id="appointmnetOnline">
+                                            <label class="form-check-label" for="appointmnetOnline">
                                                 نوبت آنلاین
                                             </label>
                                         </div>

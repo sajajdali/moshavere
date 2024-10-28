531a1adc141dcf1f0dbe042e7f24f3811e59d633
sina <sina1zamani@gmail.com>
2024-10-28 15:57:47 +0330

    add meta description

--- Modules/Front/resources/views/layouts/app.blade.php
+++ Modules/Front/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="{{ setting(Modules\Setting\Enum\SettingKeyEnum::SITE_TITLE) }}">
+    <meta name="description" content="سیستم نوبت دهی پزشکم">
     <meta name="author" content="شمیران وب">
     <meta name="keywords" content="{{ setting(Modules\Setting\Enum\SettingKeyEnum::SITE_TITLE) }}">
     <!-- Favicon -->

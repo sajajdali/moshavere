96a1a48d5614e8c6796fb379413eb4cebe86f281
sina <sina1zamani@gmail.com>
2024-10-29 12:05:43 +0330

    add store appointment for voip

--- Modules/Api/Http/Controllers/Voip/VoipController.php
+++ Modules/Api/Http/Controllers/Voip/VoipController.php
@@ -2,13 +2,23 @@
 
 namespace Modules\Api\Http\Controllers\Voip;
 
-use Hekmatinasser\Verta\Verta;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Hekmatinasser\Verta\Verta;
+use Modules\User\Entities\User;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Cache;
-use Modules\Api\Http\Controllers\Appointment\AppointmentApiController;
 use Modules\Api\Trait\ApiHandlerTrait;
+use Modules\Setting\Enum\SettingKeyEnum;
+use Modules\AppointmentUser\Enum\AppointmentVia;
+use Modules\AppointmentUser\Enum\model\UserModel;
+use Modules\AppointmentUser\Enum\model\AppointmentModel;
+use Modules\AppointmentUser\Enum\AppointmentUserKindEnum;
+use Modules\AppointmentUser\Enum\AppointmentUserTypeEnum;
+use Modules\AppointmentUser\Enum\model\UserModelAppointment;
 use Modules\AppointmentSetting\app\Models\AppointmentSetting;
+use Modules\AppointmentUser\app\Jobs\GenerateAppointmentCache;
+use Modules\Api\Http\Controllers\Appointment\AppointmentApiController;
 
 class VoipController extends Controller
 {
@@ -72,7 +82,7 @@ class VoipController extends Controller
         $findAlterNateDoctor = $appointmentApiController->findAlterNateDoctor();
 
 
-        if ($findAlterNateDoctor == null){
+        if ($findAlterNateDoctor == null) {
             return null;
         }
         $appointmentSetting = AppointmentSetting::where('user_id', $findAlterNateDoctor->id);
@@ -111,5 +121,132 @@ class VoipController extends Controller
                 'empty_times' => $this->getListEmptyAppointment($listDays),
             ]
         );
+    }
+
+    public function storeAppointment(Request $request)
+    {
+        $doctorId            = $request->get('doctor_id');
+        $visitDate           = $request->get('visit_date');
+        $mobile              = $request->get('user_mobile');
+        $placesId            = $request->get('place_id');
+        $servicesId          = $request->get('service_id');
+        $operatorId          = $request->get('operator_id');
+        $kindParameter       = $request->get('kind');
+        $description         = $request->get('description');
+        if (empty($doctorId) || empty($visitDate) || empty($mobile)) {
+            return $this->requestException([
+                'status' => false,
+                'message' => ' پزشک و تاریخ نوبت , شماره همراه کاربر الزامی است.'
+            ]);
+        }
+
+        $doctor = User::find($doctorId);
+        $startDate = Carbon::createFromTimestamp($visitDate, 'Asia/Tehran');
+        if ($startDate->isPast()) {
+            return $this->requestException([
+                'status' => false,
+                'message' => 'تاریخ انتخابی صحیح نیست.'
+            ]);
+        }
+
+        $appointmentSetting = $doctor->appointmentSettings()->active()
+            ->when(isset($servicesId), function ($q) use ($servicesId) {
+                return $q->where('service_id', $servicesId)->orWhere('service_id',null);
+            })->when(!isset($servicesId), function ($q) {
+                return $q->whereNull('service_id')->orWhereNull('service_id');
+            })->when(isset($placesId), function ($q) use ($placesId) {
+                return $q->where('place_id', $placesId)->orWhereNull('place_id');
+            })->when(! isset($placesId), function ($q) {
+                return $q->whereNull('place_id');
+            })->when(isset($operatorId), function ($q) use ($operatorId) {
+                return $q->whereNotNull('detail->ids')->whereJsonContains('detail->ids', $operatorId);
+            })->first();
+
+
+        if (! isset($appointmentSetting)) {
+            return $this->requestException([
+                'status' => false,
+                'message' => 'تنظیمات مربوط به پزشک پیدا نشد ، بخش،مطب،یا اپراتور را بررسی کنید.'
+            ]);
+        }
+        $endDate = $startDate->addMinutes($appointmentSetting->time_for_visit);
+        $user = User::where('mobile', 'LIKE', "%{$mobile}%")->first();
+        if (!isset($user)) {
+            $user  = User::create([
+                'mobile' => $mobile,
+                'password' => uniqId(),
+            ]);
+        }
+
+        $someoneModel = null;
+        $foHimself = 1;
+        // main user data
+        $mainUser = new UserModel(
+            user: $user,
+            firstName: $user->first_name,
+            lastName: $user->last_name,
+        );
+        // full user model
+        $userModelAppointment = new UserModelAppointment(userModel: $mainUser, forHimself: $foHimself, userSomeoneModel: $someoneModel);
+
+        //check if operator
+        if (isset($operatorId) && !empty($operatorId)) {
+            $oprator = null;
+            $operatorExists = User::find($operatorId);
+            if ($operatorExists) {
+                // check if oprator exixts
+                $oprator =  $operatorId;
+            }
+        } else {
+            $oprator = null;
+        }
+        if (isset($kindParameter) && ! empty($kindParameter)) {
+            $kind = AppointmentUserKindEnum::tryFrom($kindParameter);
+        } else {
+            $kind = AppointmentUserKindEnum::IN_PERSION;
+        }
+        // appointment model
+        $appointmentModel = new AppointmentModel(
+            timestamp: $startDate->timestamp,
+            appointmentVia: AppointmentVia::SELF,
+            sendSmsToUser: true,
+            serviceId: $servicesId ?? $doctor->service->first(),
+            placeId: $placesId ?? $doctor->place->first(),
+            agentId: $user->id,
+            kind: $kind,
+            smsToDoctor: false,
+            description: $description ??  '',
+            type: AppointmentUserTypeEnum::MAIN__APPOINTMENT,
+            endTime: $endDate->toTimeString(),
+        );
+
+        $detail = [];
+
+        // sms Template
+        if (
+            $appointmentSetting->detail[AppointmentSetting::PAYMENT][AppointmentSetting::STATUS] == true &&
+            $appointmentSetting->detail[AppointmentSetting::PAYMENT][AppointmentSetting::NOT_PAYING_STATUS] == 'dontSubmit'
+        ) {
+            // if payment was active
+            $detail['smsTemplate']      = setting(SettingKeyEnum::SMS_APPOINTMENT_WAITING_PAYMENT);
+        } else {
+            $detail['smsTemplate']      = setting(SettingKeyEnum::SMS_APPOINTMENT_RECEIVING_SUCCESSFUL);
+        }
+
+        $storeAppointment = app('AppointmentUserService')->storeAppointment($appointmentSetting, $userModelAppointment, $appointmentModel, $detail);
+        if ($storeAppointment['status']) {
+            GenerateAppointmentCache::dispatch($appointmentSetting);
+            return $this->ok(
+                [
+                    'status' => true,
+                    'message' => 'نوبت با موفقیت ذخیره شد'
+                ]
+            );
+        } else {
+            return $this->requestException([
+                'status' => false,
+                'message' => 'خطا در ثبت نوبت.'
+            ]);
+        }
     }
 }

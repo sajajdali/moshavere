f3871a7ed23bf252b64e92af9afede1857c2f2c6
sina <sina1zamani@gmail.com>
2024-11-19 16:08:15 +0330

    add paginate for file uploder modal

--- Modules/Admin/Resources/views/livewire/file-manager-modal.blade.php
+++ Modules/Admin/Resources/views/livewire/file-manager-modal.blade.php
@@ -1,106 +1,97 @@
 <div>
     <!-- modal -->
-    <div class="modal fade @if($isDialogShow) show @endif" data-bs-backdrop="static" id="file-selector-modal"
-         @if($isDialogShow) aria-modal="true" role="dialog" style="display: block;" @endif>
+    <div class="modal fade @if ($isDialogShow) show @endif" data-bs-backdrop="static"
+        id="file-selector-modal"
+        @if ($isDialogShow) aria-modal="true" role="dialog" style="display: block;" @endif>
         <div class="modal-dialog modal-xl modal-dialog-top" role="document">
             <div class="modal-content modal-content-demo">
                 <div class="modal-header">
                     <h6 class="modal-title">انتخاب فایل</h6>
                     <button aria-label="Close" class="btn-close" data-bs-dismiss="modal" type="button"
-                            id="btn_close_upload"><span aria-hidden="true">&times;</span></button>
+                        id="btn_close_upload"><span aria-hidden="true">&times;</span></button>
                 </div>
                 <div class="modal-body">
                     <div class="mb-2" wire:ignore>
-                        <input type="file" class="dropify" id="dropify" wire:model="uploadFile" data-height="200"/>
+                        <input type="file" class="dropify" id="dropify" wire:model="uploadFile"
+                            data-height="200" />
                     </div>
-                    <button class="btn ripple btn-secondary"
-                            id="btn_cancel_upload"
-                            wire:target="upload"
-                            wire:loading.class="op-0-3"
-                            wire:loading.attr="disabled"
-                            data-bs-dismiss="modal" type="button">انصراف
+                    <button class="btn ripple btn-secondary" id="btn_cancel_upload" wire:target="upload"
+                        wire:loading.class="op-0-3" wire:loading.attr="disabled" data-bs-dismiss="modal"
+                        type="button">انصراف
                     </button>
                 </div>
                 <div class="modal-footer justify-content-start">
                     <div class="breadcrumb-main">
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb mb-0">
-                                @foreach($broadcamp as $indexItem => $item)
-                                    @if($indexItem === 0)
+                                @foreach ($broadcamp as $indexItem => $item)
+                                    @if ($indexItem === 0)
                                         <li class="breadcrumb-item active">
-                                            <button
-                                                class="text-primary"
+                                            <button class="text-primary"
                                                 wire:click="goTo({{ $indexItem }})">{{ $item['title'] }}</button>
                                         </li>
                                     @else
                                         <li class="breadcrumb-item">
-                                            <button
-                                                wire:click="goTo({{ $indexItem }})">{{ $item['title'] }}</button>
+                                            <button wire:click="goTo({{ $indexItem }})">{{ $item['title'] }}</button>
                                         </li>
                                     @endif
                                 @endforeach
                             </ol>
                         </nav>
                     </div>
-                    @if(count($files['files']) > 0 || count($files['folders']) > 0)
+                    @if ($this->listFiles()->count() > 0)
                         <div class="table-responsive mb-3">
                             <table class="table text-nowrap text-md-nowrap table-bordered" wire:loading.class="op-0-3">
                                 <thead>
-                                <tr>
-                                    <th scope="col" style="width: 32px">#</th>
-                                    <th scope="col">نام</th>
-                                    <th scope="col" style="width: 64px">انتخاب</th>
-                                </tr>
+                                    <tr>
+                                        <th scope="col" style="width: 32px">#</th>
+                                        <th scope="col">نام</th>
+                                        <th scope="col" style="width: 64px">انتخاب</th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                @if(count($files['folders']) > 0)
-                                    @foreach($files['folders'] as $folder)
-                                        <tr style="cursor: pointer" wire:click="goToDirectory('{{ $folder }}')">
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td colspan="2"><img src="{{admin_asset('images/files/folder.png')}}"
-                                                                 alt="img"
-                                                                 style="width: 36px">
-                                                {{ $folder }}
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endif
-                                @if(count($files['files']) > 0)
-                                    @foreach($files['files'] as $fileItem)
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>
-                                                <a href="{{ $fileItem['url'] }}" target="_blank">
-                                                    <div class="d-flex align-items-center">
-                                                        <div class="me-2">
-                                                            <img
-                                                                src="{{ $fileItem['icon'] }}"
-                                                                alt="img"
-                                                                style="width: 36px">
+                                    @foreach ($this->listFiles() as $item)
+                                        @if ($item['type'] == 'folder')
+                                            <tr style="cursor: pointer"
+                                                wire:click="goToDirectory('{{ $item['name'] }}')">
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td colspan="2"><img
+                                                        src="{{ admin_asset('images/files/folder.png') }}"
+                                                        alt="img" style="width: 36px">
+                                                    {{ $item['name'] }}
+                                                </td>
+                                            </tr>
+                                        @else
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>
+                                                    <a href="{{ $item['url'] }}" target="_blank">
+                                                        <div class="d-flex align-items-center">
+                                                            <div class="me-2">
+                                                                <img src="{{ $item['icon'] }}" alt="img"
+                                                                    style="width: 36px">
+                                                            </div>
+                                                            <div>
+                                                                <h6 class="mb-1">{{ $item['name'] }}
+                                                                </h6> <span
+                                                                    class="text-muted fs-13">{{ $item['size'] }}</span>
+                                                            </div>
                                                         </div>
-                                                        <div>
-                                                            <h6 class="mb-1">{{ $fileItem['name'] }}
-                                                            </h6> <span
-                                                                class="text-muted fs-13">{{ $fileItem['size'] }}</span>
-                                                        </div>
-                                                    </div>
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <button
-                                                    type="button"
-                                                    class="btn btn-success"
-                                                    wire:target="selectFile('{{ $fileItem['url'] }}')"
-                                                    wire:loading.class.remove="btn-success"
-                                                    wire:loading.class="btn-loading disabled btn-light"
-                                                    wire:loading.attr="disabled"
-                                                    wire:click="selectFile('{{ $fileItem['url'] }}')"><i
-                                                        class="fe fe-check me-2"></i>انتخاب
-                                                </button>
-                                            </td>
-                                        </tr>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <button type="button" class="btn btn-success"
+                                                        wire:target="selectFile('{{ $item['url'] }}')"
+                                                        wire:loading.class.remove="btn-success"
+                                                        wire:loading.class="btn-loading disabled btn-light"
+                                                        wire:loading.attr="disabled"
+                                                        wire:click="selectFile('{{ $item['url'] }}')"><i
+                                                            class="fe fe-check me-2"></i>انتخاب
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @endif
                                     @endforeach
-                                @endif
                                 </tbody>
                             </table>
                         </div>
@@ -109,6 +100,9 @@
                             هیچ فایل یا پوشه ای وجود ندارد
                         </div>
                     @endif
+                    <div class="d-flex justify-content-center w-100">
+                        {{$this->listFiles()->links()}}
+                    </div>
                 </div>
             </div>
         </div>
@@ -117,18 +111,20 @@
 </div>
 @push('scripts')
     <!--Internal Fileuploads js-->
-    <script src="{{admin_asset('plugins/fileuploads/js/fileupload.js')}}"></script>
-    <script src="{{admin_asset('plugins/sweet-alert/sweetalert.min.js')}}"></script>
+    <script src="{{ admin_asset('plugins/fileuploads/js/fileupload.js') }}"></script>
+    <script src="{{ admin_asset('plugins/sweet-alert/sweetalert.min.js') }}"></script>
     <style>
-        .breadcrumb-item + .breadcrumb-item:before {
+        .breadcrumb-item+.breadcrumb-item:before {
             transform: rotate(180deg);
         }
     </style>
     <script>
         var dropifyInput;
-        $(document).ready(function () {
+        $(document).ready(function() {
             dropifyInput = $('#dropify').dropify({
-                allowedFileExtensions: ['jpg', 'jpeg', 'png', 'pdf', 'doc', 'docx', 'txt', 'xlsx', 'xls', 'zip', 'mp4'],
+                allowedFileExtensions: ['jpg', 'jpeg', 'png', 'pdf', 'doc', 'docx', 'txt', 'xlsx', 'xls',
+                    'zip', 'mp4'
+                ],
                 messages: {
                     'default': 'فایل را اینجا بکشید یا کلیک کنید',
                     'replace': 'فایل را اینجا بکشید یا کلیک کنید',
@@ -141,11 +137,11 @@
             });
         });
         //on model show
-        $('#file-selector-modal').on('show.bs.modal', function (e) {
+        $('#file-selector-modal').on('show.bs.modal', function(e) {
             Livewire.dispatch('showDialog');
         });
         //on hide modal
-        $('#file-selector-modal').on('hide.bs.modal', function (e) {
+        $('#file-selector-modal').on('hide.bs.modal', function(e) {
             Livewire.dispatch('hideDialog');
         });
         Livewire.on('error_file_manager_modal', (param) => {

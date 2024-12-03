0f52bb3c6bf272aa005f01798ae01c63c301f259
sina <sina1zamani@gmail.com>
2024-12-03 13:23:20 +0330

    fb

--- Modules/Front/resources/views/layouts/components/app-sidebar.blade.php
+++ Modules/Front/resources/views/layouts/components/app-sidebar.blade.php
@@ -8,7 +8,7 @@
             <a href="{{ route('front.aboutUs') }}">درباره ما</a>
         </li>
         <li class="sidebar__menu-item">
-            <a href="{{ route('front.contactUs') }}">ارتباط با ما</a>
+            <a href="{{ route('front.contactUs') }}">درخواست مشاوره</a>
         </li>
         <li class="sidebar__menu-item">
             <a href=""{{ route('front.searchPage', ['query' => 'پزشکان']) }}">لیست پزشکان</a>

7b6d61e0d5f7b7aa66a41176c3a547153fc5a4d3
sina <sina1zamani@gmail.com>
2024-12-10 13:03:20 +0330

    fb

--- Modules/AppointmentUser/Livewire/Admin/Online/MessageDetail.php
+++ Modules/AppointmentUser/Livewire/Admin/Online/MessageDetail.php
@@ -307,9 +307,9 @@ class MessageDetail extends Component
     public function mount()
     {
         if (
-            !auth()->user()->can('appointment_user') &&
-            !auth()->user()->can('appointment_user.own') &&
-            !auth()->user()->can('appointment_user.message') &&
+            !auth()->user()->can('appointment_user') ||
+            !auth()->user()->can('appointment_user.own') ||
+            !auth()->user()->can('appointment_user.message') ||
             !auth()->user()->can('appointment_user.online')
         ) {
             abort(403, 'Unauthorized');

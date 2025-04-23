44c5509493c65b8870e8b0287945a37e146e7553
sina <sina1zamani@gmail.com>
2025-04-23 16:52:45 +0330

    fb

--- Modules/User/Resources/views/livewire/admin/user/user-list.blade.php
+++ Modules/User/Resources/views/livewire/admin/user/user-list.blade.php
@@ -174,7 +174,7 @@
                                                                     <img alt="{{ $user->full_name }}"
                                                                         title="{{ $user->full_name }}"
                                                                         class="rounded-circle avatar-md"
-                                                                        src="{{ url('storage/'.$user->avatar) ?? asset('assets/admin/images/svgs/user.svg') }}">
+                                                                        src="{{ $user->avatar ? url('storage/'.$user->avatar) : asset('assets/admin/images/svgs/user.svg') }}">
                                                                 </a>
                                                             </div>
                                                         </div>

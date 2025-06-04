fc57ff8eb501d9b46c798a2c8843b3b8a57114ba
sina <sina1zamani@gmail.com>
2025-06-04 17:14:29 +0330

    fb

--- Modules/AppointmentUser/resources/views/livewire/admin/add-appointment/specific-day-available-appointment.blade.php
+++ Modules/AppointmentUser/resources/views/livewire/admin/add-appointment/specific-day-available-appointment.blade.php
@@ -223,7 +223,7 @@
     <livewire:appointmentuser::admin.add-appointment.modal.service-and-doctor-modal :appId="$fetchData['appId']" :appTime="$fetchData['time']"
         :serviceId="$fetchData['service']->id" :placeId="$fetchData['place']" :segmentId="$fetchData['segment']" />
     <livewire:appointmentuser::admin.add-appointment.modal.specific-day-appointment-registration-modal :appId="$fetchData['appId']"
-        :appTime="$fetchData['time']" :serviceId="$fetchData['service']->id" :placeId="$fetchData['place']" :segmentId="$fetchData['segment']" />
+        :appTime="$fetchData['time']" :serviceId="$fetchData['service']->id" :placeId="$fetchData['place']->id" :segmentId="$fetchData['segment']" />
     <div>
         @include('appointmentuser::components.appointmentlist.disapprovemodal')
     </div>

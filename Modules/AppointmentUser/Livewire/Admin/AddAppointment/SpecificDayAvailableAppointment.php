6336878eb7f0ec881801c8555ddc2ff13cc0c68b
sina <sina1zamani@gmail.com>
2025-08-09 17:43:49 +0330

    fb

--- Modules/AppointmentUser/Livewire/Admin/AddAppointment/SpecificDayAvailableAppointment.php
+++ Modules/AppointmentUser/Livewire/Admin/AddAppointment/SpecificDayAvailableAppointment.php
@@ -364,7 +364,7 @@ class SpecificDayAvailableAppointment extends Component
                 $details['segment_time'] =  $this->fetchData['segment_time'];
             }
             // create inital list aof appointment
-            $this->fetchData['RawlistOfAppointment']  = Cache::rememberForever('appointmentList.' . $app->id . '-' . $this->fethData['segment_time'], function () use ($app, $details) {
+            $this->fetchData['RawlistOfAppointment']  = Cache::rememberForever('appointmentList.' . $app->id . '-' . $this->fetchData['segment_time'], function () use ($app, $details) {
                 $app->update(['updated_log_at' => \now()]);
                 return app('AppointmentUserService')->listAppointments($app, $details);
             });

